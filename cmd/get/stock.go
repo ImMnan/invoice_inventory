4cef18194f9ed9159cf5dcbd752e450074492915
Manan patel <86557286+ImMnan@users.noreply.github.com>
2025-07-20 14:51:35 +0530

    update

--- cmd/get/stock.go
+++ cmd/get/stock.go
@@ -1,7 +1,9 @@
 package get
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -10,11 +12,14 @@ import (
 var stockCmd = &cobra.Command{
 	Use:     "stock <product_id>",
 	Short:   "Get stock details for a product",
-	Aliases: []string{"stocks", "stk"}, // 👈 Add "stocks" as an alias
+	Aliases: []string{"stocks", "stk"}, // added alias
 	Long:    `Fetches stock information for the given product ID.`,
-	Args:    cobra.ExactArgs(1), // Ensures exactly one argument is passed
+	Args:    cobra.ExactArgs(1), // Ensures exactly one argument is accepted
 	Run: func(cmd *cobra.Command, args []string) {
 		productID := args[0]
+		if productID == "all" {
+			getStock()
+		}
 		fmt.Printf("Fetching stock details for product ID: %s\n", productID)
 		// Add your logic here to fetch and display stock info
 	},
@@ -24,4 +29,28 @@ func init() {
 	stockCmd.Flags().BoolP("help", "h", false, "Help message for toggle")
 	GetCmd.AddCommand(stockCmd)
 
+}
+
+func getStock() {
+	// Open the CSV file
+	file, err := os.Open("Data/inventory.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	// Read the CSV data
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = -1 // Allow variable number of fields
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the CSV data
+	for _, row := range data {
+		for _, col := range row {
+			fmt.Printf("%s,", col)
+		}
+		fmt.Println()
+	}
 }
